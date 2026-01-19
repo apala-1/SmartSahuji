386ebeef2fb46e12f1e1b7b43c66efb7b747d4c4
anjaliofficial <bistaanjali1415@gmail.com>
2026-01-19 08:29:14 +0545

    Frontend

---
+++ frontend/react-app/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar/Navbar";
+import LandingPage from "./pages/Landing/LandingPage";
+import Login from "./pages/Login/Login";
+import Signup from "./pages/Signup/Signup";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
